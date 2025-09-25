9d20acf8fe5551f45fc36baeb9af95fbead3790e
joaopaulomendes95 <139879737+joaopaulomendes95@user.noreply.github.com>
2025-09-25 19:31:52 +0100

    add: night-owl

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,29 @@
 return {
   -- Make TokyoNight Transparent
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   opts = {
+  --     transparent = true,
+  --     styles = {
+  --       sidebars = "transparent",
+  --       floats = "transparent",
+  --     },
+  --   },
+  -- }
   {
-    "folke/tokyonight.nvim",
+    "oxfist/night-owl.nvim",
     opts = {
-      transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
+      bold = true,
+      italics = false,
+      underline = true,
+      undercurl = true,
+      transparent_background = true,
     },
-  }
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "night-owl",
+    },
+  },
 }
